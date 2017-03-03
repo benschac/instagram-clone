3797af0a7709c3a44d4d9cfd8ba0daedf4afab02
Bejamin Schachter <benschac@gmail.com>
2017-03-02 22:17:40 -0500

    comments out all functionality to test

--- app.js
+++ app.js
@@ -3,19 +3,21 @@ const app = express();
 // const config = require('./config');
 
 
-var ig = require('instagram-node').instagram();
+// var ig = require('instagram-node').instagram();
 
 // Every call to `ig.use()` overrides the `client_id/client_secret`
 // or `access_token` previously entered if they exist.
+// console.log("process: ", process.env.ACCESS_TOKEN,'afs')
+// console.log('insta: ',ig,  process.env.ACCESS_TOKEN);
+// ig.use({ access_token: process.env.ACCESS_TOKEN,
+//          client_id: process.env.CLIENT_ID,
+//          client_secret: process.env.SECRET });
 
-console.log(process.env.ACCESS_TOKEN);
-ig.use({ access_token: process.env.ACCESS_TOKEN,
-         client_id: process.env.CLIENT_ID,
-         client_secret: process.env.SECRET });
 
 
+// app.use(express.static(__dirname + '/public'));
+app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
 
@@ -24,13 +26,18 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
 
-  ig.user_self_media_recent((err, medias, pagination, remaining) => {
-    if(err) console.error(err);
+  // ig.user_self_media_recent((err, medias, pagination, remaining) => {
+  //   if(err) console.error(err);
 
 
-    res.render('pages/index', {grams: medias});
-  })
+    res.render('pages/index');
+
 
 });
 
-module.exports = app;
+app.listen(app.get('port'), () => {
+  console.log('Node app is running on port: ', app.get('port'));
+})
+
+
+// module.exports = app;
---
+++ views/pages/something.js
@@ -0,0 +1,9 @@
+<%# <% grams.forEach((gram) => { %>%>
+  // <li class="thumbnail-item">
+  //   <a href="'img/otter1.jpg" data-image-role="trigger"
+  //                             data-image-title="<%= gram.caption %>"
+  //                             data-image-url="<%= gram.images.standard_resolution.url %>">
+  //     <img class="thumbnail-image" src="<%= gram.images.low_resolution.url %>">
+  //   </a>
+  // </li>
+<%# <% }); %>%>
